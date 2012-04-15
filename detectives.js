f9deca61ec03878e13c594a0c277eb45f81ed9e0
David Thompson <dtt101@gmail.com>
2012-04-15 20:42:20 +0100

    basic list of detectives and templates

--- detectives.js
+++ detectives.js
@@ -1,19 +1,29 @@
+// Set up a collection of detectives
+Detectives = new Meteor.Collection("detectives");
+
 if (Meteor.is_client) {
-  Template.hello.greeting = function () {
-    return "Welcome to detectives.";
+  Template.thelist.detectives = function () {
+	  return Detectives.find();
   };
 
-  Template.hello.events = {
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  };
+  //Template.hello.events = {
+  //  'click input' : function () {
+  //    // template data, if any, is available in 'this'
+  //    if (typeof console !== 'undefined')
+  //      console.log("You pressed the button");
+  //  }
+  //};
 }
 
 if (Meteor.is_server) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    if (Detectives.find().count() === 0) {
+	    var detective_data = ["Sherlock Holmes",
+	                 "Hercule Poirot",
+	                 "Miss Marple",
+	                 "Father Brown"];		
+	     for (var i = 0; i < detective_data.length; i++)
+	       Detectives.insert({name: detective_data[i], votes: 0});
+    }
   });
 }
