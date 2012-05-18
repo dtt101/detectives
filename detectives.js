c345947f40b55fc697392143c819ba2dd21cd6c8
David Thompson <dtt101@gmail.com>
2012-05-18 09:58:33 +0100

    added twitter and removed selection to vote

--- detectives.js
+++ detectives.js
@@ -6,52 +6,44 @@
 Detectives = new Meteor.Collection("detectives");
 
 if (Meteor.is_client) {
-	
+
+	Meteor.startup(function () {
+		// set session if already voted
+  	if (getVoted()) {
+  		Session.set("session_voted", getVoted());
+  	}
+  });	
 	// storage helpers (uses amplify.js)
 	// set that the user has voted
 	var setVoted = function() {
 		amplify.store("voted", true );
-	}
+	};
 
 	// TODO - remove
 	var setNotVoted = function() {
+		Session.set("session_voted", undefined);
 		amplify.store("voted", false );
-	}
+	};
 	
 	// return vote state
 	var getVoted = function() {
 		return amplify.store("voted");
-	}
+	};
 	
 	// template properties
   Template.thelist.detectives = function () {
 	  return Detectives.find({}, {sort: {votes: -1, name: 1}});
   };
   
-  Template.thelist.selected_name = function () {
-     var detective = Detectives.findOne(Session.get("selected_detective"));
-     return detective && detective.name;
-  };
-
-  Template.detective.selected = function () {
-    return Session.equals("selected_detective", this._id) ? "selected" : '';
-  };
-  
   Template.detective.voted = function () {
-		return getVoted();
-  }
-  
-  Template.thelist.events = {
-    'click input.vote': function () {
-			Meteor.call('vote', Session.get("selected_detective"));
-			setVoted();
-    }
+  		return Session.get("session_voted");
   };
   
-  // sets selected detective in session
   Template.detective.events = {
-    'click': function () {
-      Session.set("selected_detective", this._id);
+    'click input.vote': function () {
+			Meteor.call('vote', this._id); // update db
+			Session.set("session_voted", this._id); // update reactive session
+			setVoted(this._id); // update local storage
     }
   };
 }
